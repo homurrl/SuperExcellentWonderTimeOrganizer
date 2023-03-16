750697ab300eb1cba4967090856e8fd3d216b671
Brea Grindley <breagrindley@Breas-MacBook-Pro.local>
2023-03-16 13:08:09 +1030

    added additional timeblocks

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,8 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
+
+//const dayjs = require("dayjs");
+
 // in the html.
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
@@ -26,6 +29,16 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+
+
+  var currentHour = dayjs().format("H")
+  console.log(currentHour)
+
+  if (currentHour=10){
+  $("#hour-10").attr("class","present")
+  }
+
+
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
