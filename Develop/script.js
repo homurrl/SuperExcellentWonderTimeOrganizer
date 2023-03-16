cf6c261235f810af1f9271845a483af3fbed80ed
Brea Grindley <breagrindley@Breas-MacBook-Pro.local>
2023-03-16 12:19:22 +1030

    jquery added to save and render input data

--- Develop/script.js
+++ Develop/script.js
@@ -8,6 +8,18 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+
+  //jquery to save and render data from local storage
+
+  $(".saveBtn").on("click",function(){
+    let text=$("#hour-10-a").val()
+    localStorage.setItem("hour10",text)
+  })
+
+  $("#hour-10-a").val(localStorage.getItem("hour10"))
+
+ 
+
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
